fea85279f6eb5a483e268ed7010c154a7d3717bd
Omar Montoya <omarx372@hotmail.com>
2021-01-13 12:54:28 +0100

    feat: add changes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artistas: any[]=[]; 
+  
+  constructor(private spotify: SpotifyService) { }
 
   ngOnInit(): void {
   }
 
-  buscar(termino:String){
-    console.log(termino)
+  buscar(termino:string){
+    this.spotify.getArtista(termino).subscribe((data:any) => {
+    this.artistas=data.artists.items;
+    })
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,16 +12,19 @@ export class SpotifyService {
 
   getNewRelease(){
    
-
     const headers= new HttpHeaders({
-      'Authorization': 'Bearer BQCPmT2TihZdMgzMTDRZT8wfJCR2bdsUjVmPfEYrPBwditUZkb4XgBsxqV6xDeWGPOVDQbtCdMsxmjt40BA'
+      'Authorization': 'Bearer BQDWytG01fWCCBav8hB0OH08FIIev6HbO5DQiiKLURUNHEeTHhx3h1nXM4PDaS0zN393GxbzTIKCcBIdukrkbFeNJi0hWsMvuLJ2KjKq_CUyYMP6ToxEw9O9tSv5s3XFzwq9H6LPvCzFFEXt00OBUt-Umhnxo4Ov_Q0'
     });
 
-   return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
- }
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+  }
 
  getArtista(termino:string){
-    
+    const headers= new HttpHeaders({
+      'Authorization': 'Bearer BQDWytG01fWCCBav8hB0OH08FIIev6HbO5DQiiKLURUNHEeTHhx3h1nXM4PDaS0zN393GxbzTIKCcBIdukrkbFeNJi0hWsMvuLJ2KjKq_CUyYMP6ToxEw9O9tSv5s3XFzwq9H6LPvCzFFEXt00OBUt-Umhnxo4Ov_Q0'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist`, { headers }); 
  }
 
 }
